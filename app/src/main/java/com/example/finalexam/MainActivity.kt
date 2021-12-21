03127e971351e225d89a881e23d053bab4f6f228
kg.khalilov.alim.dht-2.finalexam <khalilov_a@auca.kg>
2021-12-21 16:39:32 +0600

    commit

---
+++ app/src/main/java/com/example/finalexam/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.finalexam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.plusminus)
+
+        textView.setOnClickListener{
+            startActivity(Intent(this,friedrice_two_pieces::class.java))
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/finalexam/friedrice_two_pieces.kt
@@ -0,0 +1,11 @@
+package com.example.finalexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class friedrice_two_pieces : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friedrice_two_pieces)
+    }
+}
